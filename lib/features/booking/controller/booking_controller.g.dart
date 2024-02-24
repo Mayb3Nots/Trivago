180f580c9ae9fb483ba680de2f1099afb147da29
Daniel <danielongzhien@gmail.com>
2024-02-24 10:12:38 +0800

    Fixed version 1

---
+++ lib/features/booking/controller/booking_controller.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'booking_controller.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$bookingControllerHash() => r'36738bad624480814f08a6af50ce5bc1d466a67a';
+
+/// See also [BookingController].
+@ProviderFor(BookingController)
+final bookingControllerProvider =
+    AutoDisposeNotifierProvider<BookingController, HomeState>.internal(
+  BookingController.new,
+  name: r'bookingControllerProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$bookingControllerHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$BookingController = AutoDisposeNotifier<HomeState>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
